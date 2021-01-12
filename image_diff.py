a9d92f1d653bd291d899470b7030f14115ecd10b
vlad-v-melnikov <vmelnikov.miis@gmail.com>
2021-01-12 16:57:25 -0500

    Initial commit

---
+++ image_diff.py
@@ -0,0 +1,103 @@
+import os
+import argparse
+import logging
+from glob import glob
+from datetime import datetime
+from PIL import Image, ImageChops, UnidentifiedImageError
+
+
+def main():
+    make_diff_dir()
+    clean_diffs()
+
+    now = datetime.now()
+    log_time = now.strftime("%Y%m%d%H%M%S")
+    logging.basicConfig(filename=f'image_diff_{log_time}.log',
+                        format='%(asctime)s - %(levelname)s - %(message)s',
+                        level=logging.INFO)
+
+    source_file_pattern, test_file_pattern, exclusion = parse_arguments()
+
+    try:
+        source_images = glob(source_file_pattern)
+        assert len(source_images) > 0, "Could not find the source files. Exiting."
+        test_images = glob(test_file_pattern)
+        assert len(test_images) > 0, "Could not find the target files. Exiting."
+    except AssertionError as e:
+        print(e)
+        logging.error(e)
+        return
+
+    if exclusion:
+        source_images = exclude_images(source_images, exclusion)
+        test_images = exclude_images(test_images, exclusion)
+
+    print(f'{len(source_images)} benchmark images, {len(test_images)} target images.')
+    logging.info(f'{len(source_images)} benchmark images, {len(test_images)} target images.')
+
+    screens = zip(source_images, test_images)
+    for source, target in screens:
+        try:
+            image_source = Image.open(source).convert('RGB')
+            image_test = Image.open(target).convert('RGB')
+        except UnidentifiedImageError:
+            print(f"Cannot process image {source} or {target}")
+            logging.error(f'Cannot process image {source} or {target}')
+            continue
+
+        diff = ImageChops.difference(image_source, image_test)
+        if bool(diff.getbbox()):
+            identifier = os.path.basename(source)
+            print(f'{target} DOES NOT match {source}')
+            logging.warning(f'{target} DOES NOT match {source}')
+            diff.save('diff/diff_' + identifier + '.gif')
+
+
+def get_image_list(file_pattern, purpose, echo=True):
+    try:
+        source_images = glob(file_pattern)
+        assert len(source_images) > 0, f"Could not find the {purpose} files. Exiting."
+        return source_images
+    except AssertionError as e:
+        if echo:
+            print(e)
+        logging.error(e)
+        raise e
+
+
+def exclude_images(images, exclusion):
+    images = [name for name in images if exclusion not in name]
+    return images
+
+
+def make_diff_dir(echo=True):
+    if not os.path.exists("./diff"):
+        os.makedirs("./diff")
+        if echo:
+            print("Created diff folder.")
+
+
+def clean_diffs(echo=True):
+    files = glob("./diff/*.*")
+    for f in files:
+        os.unlink(f)
+    if len(files) > 0:
+        if echo:
+            print(f"Cleared diff folder. Deleted {len(files)} file(s).")
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--source', help="These files are the originals - they are correct")
+    parser.add_argument('-t', '--target', help="These files are target images - we are not sure if they are correct")
+    parser.add_argument('-x', '--exclude', help="A string for what these files names SHOULD NOT contain")
+    args = parser.parse_args()
+    source_file_pattern = args.source if args.source else ".\\source\\*.*"
+    target_file_pattern = args.target if args.target else ".\\target\\*.*"
+    exclusion = args.exclude if args.exclude else ''
+
+    return source_file_pattern, target_file_pattern, exclusion
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/test_image_diff.py
@@ -0,0 +1,75 @@
+import unittest
+import os
+import image_diff
+
+
+class TestImageDiff(unittest.TestCase):
+    def setUp(self) -> None:
+        self.files = ['one.gif', 'two.gif', 'three_nc.gif']
+        self.files.sort()
+
+    def test_exclude_images_with_exclusion(self):
+        original = self.files
+        exclude = 'nc.gif'
+        result = image_diff.exclude_images(original, exclude)
+        self.assertListEqual(result, ['one.gif', 'two.gif'])
+
+    def test_exclude_images_no_exclusion(self):
+        original = ['one.gif', 'two.gif', 'three.gif']
+        exclude = 'nc.gif'
+        result = image_diff.exclude_images(original, exclude)
+        self.assertListEqual(result, original)
+
+    def test_get_image_list(self):
+        for name in self.files:
+            with open(name, 'w') as f:
+                f.write(f"name")
+
+        result = image_diff.get_image_list('*.gif', 'source', echo=False)
+
+        self.assertListEqual(self.files, result)
+        for name in self.files:
+            if os.path.exists(name):
+                os.unlink(name)
+
+    def test_get_image_list_exception(self):
+        for name in self.files:
+            if os.path.exists(name):
+                os.unlink(name)
+        with self.assertRaises(AssertionError):
+            image_diff.get_image_list('*.gif', 'source', echo=False)
+
+    def test_make_diff_dir_no_dir(self):
+        was_no_dir = True
+        if os.path.isdir("diff"):
+            was_no_dir = False
+            os.rmdir("diff")
+        image_diff.make_diff_dir(echo=False)
+
+        self.assertTrue(os.path.isdir("diff"))
+
+        if was_no_dir:
+            os.rmdir("diff")
+
+    def test_clean_diffs_with_files(self):
+        was_no_dir = False
+        if not os.path.exists("diff"):
+            os.mkdir("diff")
+            was_no_dir = True
+        for name in self.files:
+            with open(f"diff/{name}", 'w') as f:
+                f.write(f"{name}")
+
+        image_diff.clean_diffs(echo=False)
+        for name in self.files:
+            self.assertFalse(os.path.exists(f"diffs/{name}"))
+
+        if was_no_dir:
+            os.rmdir("diff")
+
+    def tearDown(self) -> None:
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
